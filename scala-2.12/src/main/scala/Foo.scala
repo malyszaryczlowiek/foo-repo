92334112946fe08ba1e805a3367862669a88bbe5
malyszaryczlowiek <45798727+malyszaryczlowiek@users.noreply.github.com>
2022-09-12 19:41:57 +0200

    Deleting

--- scala-2.12/src/main/scala/Foo.scala
+++ scala-2.12/src/main/scala/Foo.scala
@@ -5,7 +5,6 @@ package io.github.malyszaryczlowiek
  * Simplest Foo class
  * This is editted by someone else.
  * Now i have added something.
- * Onother new line
  */
 class Foo {
   override def toString: String = s"I'm Foo."
