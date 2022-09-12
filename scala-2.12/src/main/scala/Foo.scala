ec867622b151b262c0f547ce918bd778b2795814
malyszaryczlowiek <45798727+malyszaryczlowiek@users.noreply.github.com>
2022-09-12 19:34:49 +0200

    Removed all empty asterix from comment.

--- scala-2.12/src/main/scala/Foo.scala
+++ scala-2.12/src/main/scala/Foo.scala
@@ -3,13 +3,8 @@ package io.github.malyszaryczlowiek
 
 /**
  * Simplest Foo class
- *
  * This is editted by someone else.
- *
  * Now i have added something.
- *
- *
- *
  * Onother new line
  */
 class Foo {
